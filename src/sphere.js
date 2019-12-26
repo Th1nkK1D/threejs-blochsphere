0272663503b66e42d6584bebc466c0966975fe8e
Th1nkK1D <witheep@gmail.com>
2019-12-26 13:18:22 +0700

    Add parcel

--- src/sphere.js
+++ src/sphere.js
@@ -1,3 +1,6 @@
+import * as THREE from 'three'
+import TWEEN from '@tweenjs/tween.js'
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
@@ -57,4 +60,7 @@ function rotateStateArrow(x, y, z, duration) {
         .start();
 }
 
-animate()
+window.sphere = {
+    animate,
+    rotateStateArrow,
+}
