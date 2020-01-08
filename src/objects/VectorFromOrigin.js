b8cd2f0ff5912fe9d6cea0be1d53afaef3cdd7c1
Th1nkK1D <witheep@gmail.com>
2020-01-08 21:00:32 +0700

    Refactor VectorFromOrigin as a class

---
+++ src/objects/VectorFromOrigin.js
@@ -0,0 +1,29 @@
+import { Geometry, Vector3, Line, LineBasicMaterial } from 'three'
+import TWEEN from '@tweenjs/tween.js'
+
+class VectorFromOrigin extends Line {
+    constructor(rad, psi, theta, color) {
+        const geo = new Geometry()
+
+        geo.vertices.push(
+            new Vector3(0, 0, 0),
+            new Vector3().setFromSphericalCoords(rad, psi, theta),
+        )
+
+        super(geo, new LineBasicMaterial({ color }))
+        this.state = { psi, theta }
+    }
+
+    rotateToSphericalCoords(psi, theta, duration) {
+        new TWEEN.Tween(this.state)
+            .to({ psi, theta }, duration)
+            .onUpdate(() => {
+                this.geometry.vertices[1].setFromSphericalCoords(1, this.state.psi, this.state.theta)
+                this.geometry.verticesNeedUpdate = true
+            })
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .start()
+    }
+}
+
+export default VectorFromOrigin
--- src/sphere.js
+++ src/sphere.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import TWEEN from '@tweenjs/tween.js'
 
 import UnitSphere from './objects/UnitSphere'
+import VectorFromOrigin from './objects/VectorFromOrigin'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
@@ -15,7 +16,7 @@ camera.lookAt(0, 0, 0)
 
 function run() {
     animateScene()
-    animateRipple()
+    // animateRipple()
 }
 
 function animateScene() {
@@ -24,29 +25,11 @@ function animateScene() {
 	renderer.render(scene, camera)
 }
 
-function createUnitVectorLineFromOrigin(rad, psi, theta, color) {
-    const geo = new THREE.Geometry()
-
-    geo.vertices.push(
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3().setFromSphericalCoords(rad, psi, theta),
-    )
-
-    return new THREE.Line(
-        geo,
-        new THREE.LineBasicMaterial({ color })
-    )
-}
-
-const xAxisLine = createUnitVectorLineFromOrigin(1.5, Math.PI/2, Math.PI/2, 0xff0000)
-const yAxisLine = createUnitVectorLineFromOrigin(1.5, 0, Math.PI/2, 0x00ff00)
-const zAxisLine = createUnitVectorLineFromOrigin(1.5, Math.PI/2, 0, 0x0000ff)
-
 const sphere = new UnitSphere({ color: 0xffffff, transparent: true, opacity: 0.3 })
-
-let state = { psi: Math.PI/2, theta: 0 }
-
-const stateArrow = createUnitVectorLineFromOrigin(1, state.psi, state.theta, 0xffffff)
+const xAxisLine = new VectorFromOrigin(1.5, Math.PI/2, Math.PI/2, 0xff0000)
+const yAxisLine = new VectorFromOrigin(1.5, 0, Math.PI/2, 0x00ff00)
+const zAxisLine = new VectorFromOrigin(1.5, Math.PI/2, 0, 0x0000ff)
+const stateArrow = new VectorFromOrigin(1, Math.PI/2, 0, 0xffffff)
 
 const rippleRadius = { value: 1 }
 const circleGeo = new THREE.CircleGeometry(1.01, 64)
@@ -58,7 +41,7 @@ const ripple = new THREE.LineLoop(
     new THREE.LineBasicMaterial({ color: 0xffff00, transparent: true })
 )
 
-ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
+// ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
 
 scene
     .add(sphere)
@@ -68,33 +51,29 @@ scene
     .add(stateArrow)
     .add(ripple)
 
-function rotateStateArrow(psi, theta, duration) {
-    new TWEEN.Tween(state)
-        .to({ psi, theta }, duration)
-        .onUpdate(() => {
-            stateArrow.geometry.vertices[1].setFromSphericalCoords(1, state.psi, state.theta)
-            stateArrow.geometry.verticesNeedUpdate = true
-        })
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .start()
+function rotateStateArrowBy(psi, theta) {
+    stateArrow.rotateToSphericalCoords(
+        stateArrow.state.psi + psi,
+        stateArrow.state.theta + theta,
+        1000
+    )
 }
 
-function animateRipple() {
-    new TWEEN.Tween(rippleRadius)
-        .to({ value: 0 }, 2000)
-        .onUpdate(() => {
-            ripple.lookAt(stateArrow.geometry.vertices[1])
-            ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
-            ripple.scale.setScalar(Math.sqrt(1-rippleRadius.value*rippleRadius.value))
-            ripple.material.opacity = rippleRadius.value
-        })
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .repeat(Infinity)
-        .start()
-}
+// function animateRipple() {
+//     new TWEEN.Tween(rippleRadius)
+//         .to({ value: 0 }, 2000)
+//         .onUpdate(() => {
+//             ripple.lookAt(stateArrow.geometry.vertices[1])
+//             ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
+//             ripple.scale.setScalar(Math.sqrt(1-rippleRadius.value*rippleRadius.value))
+//             ripple.material.opacity = rippleRadius.value
+//         })
+//         .easing(TWEEN.Easing.Quadratic.Out)
+//         .repeat(Infinity)
+//         .start()
+// }
 
 window.sphere = {
     run,
-    state,
-    rotateStateArrow,
+    rotateStateArrowBy,
 }
