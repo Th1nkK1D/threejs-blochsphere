8d58fb58d569e84e7195d249a8250b190ebcf095
Th1nkK1D <witheep@gmail.com>
2020-01-08 21:34:24 +0700

    Refactor Ripple as a class

---
+++ src/objects/Ripple.js
@@ -0,0 +1,37 @@
+import { LineLoop, CircleGeometry, LineBasicMaterial } from 'three'
+import TWEEN from '@tweenjs/tween.js'
+
+class Ripple extends LineLoop {
+    constructor(radius, material) {
+        const circleGeo = new CircleGeometry(1.01, 64)
+
+        circleGeo.vertices.shift()
+
+        super(
+            circleGeo,
+            new LineBasicMaterial(material)
+        )
+
+        this.rippleRadius = { value: radius }
+    }
+
+    setPositionFromSphericalCoords(psi, theta) {
+        this.position.setFromSphericalCoords(this.rippleRadius.value, psi, theta)
+    }
+
+    animateFromVector(vectorFromOrigin) {
+        new TWEEN.Tween(this.rippleRadius)
+            .to({ value: 0 }, 2000)
+            .onUpdate(() => {
+                this.lookAt(vectorFromOrigin.geometry.vertices[1])
+                this.position.setFromSphericalCoords(this.rippleRadius.value, vectorFromOrigin.state.psi, vectorFromOrigin.state.theta)
+                this.scale.setScalar(Math.sqrt(1-this.rippleRadius.value*this.rippleRadius.value))
+                this.material.opacity = this.rippleRadius.value
+            })
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .repeat(Infinity)
+            .start()
+    }
+}
+
+export default Ripple
--- src/sphere.js
+++ src/sphere.js
@@ -3,20 +3,35 @@ import TWEEN from '@tweenjs/tween.js'
 
 import UnitSphere from './objects/UnitSphere'
 import VectorFromOrigin from './objects/VectorFromOrigin'
+import Ripple from './objects/Ripple'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-camera.position.set(5, 5, 5)
-camera.lookAt(0, 0, 0)
+const sphere = new UnitSphere({ color: 0xffffff, transparent: true, opacity: 0.3 })
+const xAxisLine = new VectorFromOrigin(1.5, Math.PI/2, Math.PI/2, 0xff0000)
+const yAxisLine = new VectorFromOrigin(1.5, 0, Math.PI/2, 0x00ff00)
+const zAxisLine = new VectorFromOrigin(1.5, Math.PI/2, 0, 0x0000ff)
+const stateArrow = new VectorFromOrigin(1, Math.PI/2, 0, 0xffffff)
+const ripple = new Ripple(1, { color: 0xffff00, transparent: true })
 
 function run() {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    camera.position.set(5, 5, 5)
+    camera.lookAt(0, 0, 0)
+
+    scene
+        .add(sphere)
+        .add(xAxisLine)
+        .add(yAxisLine)
+        .add(zAxisLine)
+        .add(stateArrow)
+        .add(ripple)
+
     animateScene()
-    // animateRipple()
+    ripple.animateFromVector(stateArrow)
 }
 
 function animateScene() {
@@ -25,32 +40,6 @@ function animateScene() {
 	renderer.render(scene, camera)
 }
 
-const sphere = new UnitSphere({ color: 0xffffff, transparent: true, opacity: 0.3 })
-const xAxisLine = new VectorFromOrigin(1.5, Math.PI/2, Math.PI/2, 0xff0000)
-const yAxisLine = new VectorFromOrigin(1.5, 0, Math.PI/2, 0x00ff00)
-const zAxisLine = new VectorFromOrigin(1.5, Math.PI/2, 0, 0x0000ff)
-const stateArrow = new VectorFromOrigin(1, Math.PI/2, 0, 0xffffff)
-
-const rippleRadius = { value: 1 }
-const circleGeo = new THREE.CircleGeometry(1.01, 64)
-
-circleGeo.vertices.shift()
-
-const ripple = new THREE.LineLoop(
-    circleGeo,
-    new THREE.LineBasicMaterial({ color: 0xffff00, transparent: true })
-)
-
-// ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
-
-scene
-    .add(sphere)
-    .add(xAxisLine)
-    .add(yAxisLine)
-    .add(zAxisLine)
-    .add(stateArrow)
-    .add(ripple)
-
 function rotateStateArrowBy(psi, theta) {
     stateArrow.rotateToSphericalCoords(
         stateArrow.state.psi + psi,
@@ -58,20 +47,6 @@ function rotateStateArrowBy(psi, theta) {
         1000
     )
 }
-
-// function animateRipple() {
-//     new TWEEN.Tween(rippleRadius)
-//         .to({ value: 0 }, 2000)
-//         .onUpdate(() => {
-//             ripple.lookAt(stateArrow.geometry.vertices[1])
-//             ripple.position.setFromSphericalCoords(rippleRadius.value, state.psi, state.theta)
-//             ripple.scale.setScalar(Math.sqrt(1-rippleRadius.value*rippleRadius.value))
-//             ripple.material.opacity = rippleRadius.value
-//         })
-//         .easing(TWEEN.Easing.Quadratic.Out)
-//         .repeat(Infinity)
-//         .start()
-// }
 
 window.sphere = {
     run,
