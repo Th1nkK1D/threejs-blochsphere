3d6af67e88b2fdb0f051d3ca98f1addf07a63d37
Th1nkK1D <witheep@gmail.com>
2020-01-18 13:20:29 +0700

    Add index endpoint for components export

---
+++ index.js
@@ -0,0 +1,11 @@
+import ArcLine from './src/objects/ArcLine'
+import Ripple from './src/objects/Ripple'
+import UnitSphere from './src/objects/UnitSphere'
+import VectorFromOrigin from './src/objects/VectorFromOrigin'
+
+export default blochSphereComponents = {
+    ArcLine,
+    Ripple,
+    UnitSphere,
+    VectorFromOrigin,
+}
